3fc7955faad8002fc38550031b3f774b8026cace
Gopal-Dahale <dahalegopal27@gmail.com>
2021-08-05 20:22:29 +0530

    initialized project

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Home from '../components/home-page/home';
+import { projectsList } from '../data/projects-list';
+import PageLayout from '../components/layout/pageLayout';
+
+export default function Index({ projects }) {
+  return (
+    <PageLayout title="Muhammad Ahmad - Full Stack Developer">
+      <Home projects={projects} />
+    </PageLayout>
+  );
+}
+
+export function getStaticProps() {
+  const projects = projectsList;
+  return {
+    props: {
+      projects
+    }
+  };
+}
